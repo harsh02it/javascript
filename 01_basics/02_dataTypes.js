cf0ec22ca015050229dea6c5bfbee52a224b429d
harsh02it <harshitsavani@gmail.com>
2025-07-14 22:33:57 -0400

    learned about datatypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all code as a newer version of Javascript(ES6)
+
+// alert("Hello World!"); //we are using node.js and not a browser, so this will not work and throw an error in vscode(strictly)
+// alert can be used in a browser as javascript is executed in the browser
+
+let number = 45;
+let name = "John";
+let isActive = true;
+
+//primitive data types
+//number
+//bigInt
+//string ('') or ("")
+//boolean (true/false)
+//null => standalone value
+//undefined => variable that doesn't have any value yet. The value is not assigned yet.
+//symbol => unique value
+
+//object
+
+console.log(typeof number);
+console.log(typeof null); //object
+//null is an object in javascript (Asked in interviews)
+console.log(typeof undefined); //undefined
+//undefined is a type in javascript
